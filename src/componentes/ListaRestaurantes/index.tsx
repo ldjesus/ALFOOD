8c6036f72a82345bb614bcc5679782245f35be15
Leandro <ufmt.leandro@gmail.com>
2024-04-23 09:29:03 -0400

    alterações para api

--- src/componentes/ListaRestaurantes/index.tsx
+++ src/componentes/ListaRestaurantes/index.tsx
@@ -1,3 +1,5 @@
+import axios from 'axios';
+import { useEffect } from 'react';
 import IRestaurante from '../../interfaces/IRestaurante';
 import style from './ListaRestaurantes.module.scss';
 import Restaurante from './Restaurante';
@@ -88,6 +90,18 @@ const ListaRestaurantes = () => {
       ]
     }
   ]
+
+  useEffect(() => {
+    //obter restaurante
+    axios.get('http://0.0.0.0:8000/api/v1/restaurantes/')
+    .then(resposta => {
+      console.log(resposta)
+    })
+    .catch(erro => {
+      console.log(erro)
+    })
+  }, [])
+
 
   return (<section className={style.ListaRestaurantes}>
     <h1>Os restaurantes mais <em>bacanas</em>!</h1>
